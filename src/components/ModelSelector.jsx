738ba7a90ace4795325ae75632b8b8fd4c3be0ff
BENcao318 <labber@DESKTOP-U16DFJJ.localdomain>
2022-10-29 15:09:00 -0400

    Change the host from Heroku to Netlify

--- src/components/ModelSelector.jsx
+++ src/components/ModelSelector.jsx
@@ -1,31 +1,34 @@
-import React, { useState } from 'react';
-import { Transition } from "@headlessui/react";
-import { FaConnectdevelop } from 'react-icons/fa';
+import React, { useState } from 'react'
+import { Transition } from '@headlessui/react'
+import { FaConnectdevelop } from 'react-icons/fa'
 
 // useCallback
 export default function ModelSelector({ engine, setEngine }) {
-  const [menuIsOpen, setMenuIsOpen] = useState(false);
+  const [menuIsOpen, setMenuIsOpen] = useState(false)
 
   function toggleMenu() {
-    setMenuIsOpen(!menuIsOpen);
+    setMenuIsOpen(!menuIsOpen)
   }
 
   function onSelected(e) {
-    setEngine(e.target.id);
+    setEngine(e.target.id)
   }
 
   return (
     <ul>
       <li className="relative px-6 py-3">
         <button
-          className="place-items-center w-full text-xl font-semibold transition-colors duration-150 hover:text-gray-800  dark:hover:text-gray-200"
+          className="w-full text-xl font-semibold transition-colors duration-150 place-items-center hover:text-gray-800 dark:hover:text-gray-200"
           onClick={toggleMenu}
           aria-haspopup="true"
         >
-          <span className="inline-flex  mx-auto">
-            <div className='flex-col  md:space-x-6 lg:space-x-12  justify-between align'>
-              <div className='inline-flex'>
-                <FaConnectdevelop className='font-extrabold text-cyan-600' size={30} />
+          <span className="inline-flex mx-auto">
+            <div className="flex-col justify-between md:space-x-6 lg:space-x-12 align">
+              <div className="inline-flex">
+                <FaConnectdevelop
+                  className="font-extrabold text-cyan-600"
+                  size={30}
+                />
                 <span className="ml-4">Choose a model</span>
               </div>
               <p className="text-sm italic text-orange-600">
@@ -33,17 +36,14 @@ export default function ModelSelector({ engine, setEngine }) {
                 {engine}
               </p>
             </div>
-
           </span>
           <svg
-            className="w-12 h-12 inline-flex ml-8"
+            className="inline-flex w-12 h-12 ml-8"
             aria-hidden="true"
             fill="currentColor"
             viewBox="0 0 20 20"
           >
-            <path
-              d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-            ></path>
+            <path d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"></path>
           </svg>
         </button>
 
@@ -57,45 +57,75 @@ export default function ModelSelector({ engine, setEngine }) {
           leaveTo="opacity-0 scale-95"
         >
           <ul
-            className="form-check w-4/5 p-2 mx-auto space-y-2 overflow-hidden text-lg font-medium text-gray-500 rounded-md shadow-inner bg-gray-50 dark:text-gray-400 dark:bg-gray-900"
+            className="w-4/5 p-2 mx-auto space-y-2 overflow-hidden text-lg font-medium text-gray-500 rounded-md shadow-inner form-check bg-gray-50 dark:text-gray-400 dark:bg-gray-900"
             aria-label="submenu"
           >
-            <li
-              className="px-2 py-1 transition-colors duration-150 space-x-12 hover:text-gray-800 dark:hover:text-gray-200"
-            >
-              <input className="form-check-input appearance-none rounded-full h-6 w-6 border-2 checked:border-8 border-gray-600 bg-white checked:bg-white-600 checked:border-orange-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mx-auto cursor-pointer peer" type="radio" name="option" id="text-davinci-002" onChange={onSelected} checked={engine === 'text-davinci-002'} />
-              <label className="form-check-label inline-block text-gray-800" htmlFor="text-davinci-002">
+            <li className="px-2 py-1 space-x-12 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200">
+              <input
+                className="float-left w-6 h-6 mx-auto mt-1 align-top transition duration-200 bg-white bg-center bg-no-repeat bg-contain border-2 border-gray-600 rounded-full appearance-none cursor-pointer form-check-input checked:border-8 checked:bg-white-600 checked:border-orange-600 focus:outline-none peer"
+                type="radio"
+                name="option"
+                id="text-davinci-002"
+                onChange={onSelected}
+                checked={engine === 'text-davinci-002'}
+              />
+              <label
+                className="inline-block text-gray-800 form-check-label"
+                htmlFor="text-davinci-002"
+              >
                 Davinci
               </label>
             </li>
-            <li
-              className="px-2 py-1 transition-colors duration-150 space-x-12 hover:text-gray-800 dark:hover:text-gray-200"
-            >
-              <input className="form-check-input appearance-none rounded-full h-6 w-6 border-2 checked:border-8 border-gray-600 bg-white checked:bg-white-600 checked:border-orange-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mx-auto cursor-pointer peer" type="radio" name="option" id="text-curie-001" onChange={onSelected} checked={engine === 'text-curie-001'} />
-              <label className="form-check-label inline-block text-gray-800" htmlFor="text-curie-001">
+            <li className="px-2 py-1 space-x-12 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200">
+              <input
+                className="float-left w-6 h-6 mx-auto mt-1 align-top transition duration-200 bg-white bg-center bg-no-repeat bg-contain border-2 border-gray-600 rounded-full appearance-none cursor-pointer form-check-input checked:border-8 checked:bg-white-600 checked:border-orange-600 focus:outline-none peer"
+                type="radio"
+                name="option"
+                id="text-curie-001"
+                onChange={onSelected}
+                checked={engine === 'text-curie-001'}
+              />
+              <label
+                className="inline-block text-gray-800 form-check-label"
+                htmlFor="text-curie-001"
+              >
                 Curie
               </label>
             </li>
-            <li
-              className="px-2 py-1 transition-colors duration-150 space-x-12 hover:text-gray-800 dark:hover:text-gray-200"
-            >
-              <input className="form-check-input appearance-none rounded-full h-6 w-6 border-2 checked:border-8 border-gray-600 bg-white checked:bg-white-600 checked:border-orange-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mx-auto cursor-pointer peer" type="radio" name="option" id="text-babbage-001" onChange={onSelected} checked={engine === 'text-babbage-001'} />
-              <label className="form-check-label inline-block text-gray-800" htmlFor="text-babbage-001">
+            <li className="px-2 py-1 space-x-12 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200">
+              <input
+                className="float-left w-6 h-6 mx-auto mt-1 align-top transition duration-200 bg-white bg-center bg-no-repeat bg-contain border-2 border-gray-600 rounded-full appearance-none cursor-pointer form-check-input checked:border-8 checked:bg-white-600 checked:border-orange-600 focus:outline-none peer"
+                type="radio"
+                name="option"
+                id="text-babbage-001"
+                onChange={onSelected}
+                checked={engine === 'text-babbage-001'}
+              />
+              <label
+                className="inline-block text-gray-800 form-check-label"
+                htmlFor="text-babbage-001"
+              >
                 Babbage
               </label>
             </li>
-            <li
-              className="px-2 py-1 transition-colors duration-150 space-x-12 hover:text-gray-800 dark:hover:text-gray-200"
-            >
-              <input className="form-check-input appearance-none rounded-full h-6 w-6 border-2 checked:border-8 border-gray-600 bg-white checked:bg-white-600 checked:border-orange-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mx-auto cursor-pointer peer" type="radio" name="option" id="text-adda-001" onChange={onSelected} checked={engine === 'text-adda-001'} />
-              <label className="form-check-label inline-block text-gray-800" htmlFor="text-adda-001">
+            <li className="px-2 py-1 space-x-12 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200">
+              <input
+                className="float-left w-6 h-6 mx-auto mt-1 align-top transition duration-200 bg-white bg-center bg-no-repeat bg-contain border-2 border-gray-600 rounded-full appearance-none cursor-pointer form-check-input checked:border-8 checked:bg-white-600 checked:border-orange-600 focus:outline-none peer"
+                type="radio"
+                name="option"
+                id="text-adda-001"
+                onChange={onSelected}
+                checked={engine === 'text-adda-001'}
+              />
+              <label
+                className="inline-block text-gray-800 form-check-label"
+                htmlFor="text-adda-001"
+              >
                 Adda
               </label>
             </li>
-
           </ul>
         </Transition>
-
       </li>
     </ul>
   )
